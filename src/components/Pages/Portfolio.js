26f09684784b0c032e952a29187367534d1ee547
Ghulam Hassan <ghkroshnal@gmail.com>
2024-10-26 10:17:56 +0500

    Bootstrap5 as saas with react js
    This is a profile website code designed using bootstrap 5 as sass with react 5.

---
+++ src/components/Pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h2>Wait it is comming</h2>
+    </div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/Pages/Testimonials.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+      <h2>It will come soon</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur architecto magni, dicta maxime laborum temporibus dolorem esse doloremque illo quas nisi enim molestias. Tempore ducimus molestiae in dolore enim.
+      </p>
+    </div>
+  )
+}
+
+export default Testimonials
